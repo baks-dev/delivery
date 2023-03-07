7de278f0864b69af1a5f30001f283a357526d125
baks.dev <admin@baks.dev>
2023-03-07 21:01:53 +0300

    Baks Development

---
+++ Entity/Event/DeliveryEvent.php
@@ -0,0 +1,156 @@
+<?php
+/*
+ *  Copyright 2023.  Baks.dev <admin@baks.dev>
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is furnished
+ *  to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
+
+declare(strict_types=1);
+
+namespace BaksDev\Delivery\Entity\Event;
+
+use App\System\Type\Locale\Locale;
+use BaksDev\Core\Type\Modify\ModifyAction;
+use BaksDev\Core\Type\Modify\ModifyActionEnum;
+use BaksDev\Delivery\Entity\Modify\DeliveryModify;
+use BaksDev\Delivery\Event\DeliveryEventUid;
+use BaksDev\Delivery\Id\DeliveryUid;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\DBAL\Types\Types;
+use BaksDev\Core\Entity\EntityEvent;
+use BaksDev\Core\Entity\EntityState;
+use InvalidArgumentException;
+
+/* DeliveryEvent */
+
+
+#[ORM\Entity]
+#[ORM\Table(name: 'delivery_event')]
+class DeliveryEvent extends EntityEvent
+{
+	public const TABLE = 'delivery_event';
+	
+	/** ID */
+	#[ORM\Id]
+	#[ORM\Column(type: DeliveryEventUid::TYPE)]
+	private DeliveryEventUid $id;
+	
+	/** ID Delivery */
+	#[ORM\Column(type: DeliveryUid::TYPE, nullable: false)]
+	private ?DeliveryUid $main = null;
+	
+	/** One To One */
+	//#[ORM\OneToOne(mappedBy: 'event', targetEntity: DeliveryLogo::class, cascade: ['all'])]
+	//private ?DeliveryOne $one = null;
+	
+	/** Модификатор */
+	#[ORM\OneToOne(mappedBy: 'event', targetEntity: DeliveryModify::class, cascade: ['all'])]
+	private DeliveryModify $modify;
+	
+	/** Перевод */
+	//#[ORM\OneToMany(mappedBy: 'event', targetEntity: DeliveryTrans::class, cascade: ['all'])]
+	//private Collection $translate;
+	
+	public function __toString() : string
+	{
+		return $this->id;
+	}
+	
+	
+	public function __construct()
+	{
+		$this->id = new DeliveryEventUid();
+		$this->modify = new DeliveryModify($this);
+		
+	}
+	
+	
+	public function __clone()
+	{
+		$this->id = new DeliveryEventUid();
+	}
+	
+	
+	public function getId() : DeliveryEventUid
+	{
+		return (string) $this->id;
+	}
+	
+	
+	public function setMain(DeliveryUid|Delivery $main) : void
+	{
+		$this->main = $main instanceof Delivery ? $main->getId() : $main;
+	}
+	
+	
+	public function getMain() : ?DeliveryUid
+	{
+		return $this->main;
+	}
+	
+	
+	public function getDto($dto) : mixed
+	{
+		if($dto instanceof DeliveryEventInterface)
+		{
+			return parent::getDto($dto);
+		}
+		
+		throw new InvalidArgumentException(sprintf('Class %s interface error', $dto::class));
+	}
+	
+	
+	public function setEntity($dto) : mixed
+	{
+		if($dto instanceof DeliveryEventInterface)
+		{
+			return parent::setEntity($dto);
+		}
+		
+		throw new InvalidArgumentException(sprintf('Class %s interface error', $dto::class));
+	}
+	
+	
+	public function isModifyActionEquals(ModifyActionEnum $action) : bool
+	{
+		return $this->modify->equals($action);
+	}
+	
+	//	public function getUploadClass() : DeliveryImage
+	//	{
+	//		return $this->image ?: $this->image = new DeliveryImage($this);
+	//	}
+	
+	//	public function getNameByLocale(Locale $locale) : ?string
+	//	{
+	//		$name = null;
+	//		
+	//		/** @var DeliveryTrans $trans */
+	//		foreach($this->translate as $trans)
+	//		{
+	//			if($name = $trans->name($locale))
+	//			{
+	//				break;
+	//			}
+	//		}
+	//		
+	//		return $name;
+	//	}
+}
---
+++ region.php
@@ -0,0 +1,154 @@
+<?php
+
+/*
+<option value="1998532">Адыгея</option>
+
+https://mosautoshina.ru/ajax/changeregion.php?region=1998532
+</option>	<option value="Адыгейск">Адыгейск</option>
+<option value="Майкоп">Майкоп</option>
+
+
+Доставка
+Поля для заполнения пользователем
+
+
+
+<option value="3160">Алтайский край</option>
+https://mosautoshina.ru/ajax/changeregion.php?region=3160
+<option value="Алтайское">Алтайское</option>
+		<option value="Ая">Ая</option>
+		<option value="Барнаул">Барнаул</option>
+		<option value="Белово">Белово</option>
+		<option value="Белокуриха">Белокуриха</option>
+		<option value="Бийск">Бийск</option>
+		<option value="Горно-Алтайск">Горно-Алтайск</option>
+		<option value="Заринск">Заринск</option>
+		<option value="Камень-на-Оби">Камень-на-Оби</option>
+		<option value="Новоалтайск">Новоалтайск</option>
+		<option value="Рубцовск">Рубцовск</option>
+		<option value="Славгород">Славгород</option>
+	<option disabled="disabled">------------------------------</option>
+
+		<option value="Акташ">Акташ</option>
+		<option value="Акутиха">Акутиха</option>
+		<option value="Алтайский">Алтайский</option>
+		<option value="Баево">Баево</option>
+		<option value="Белоярск">Белоярск</option>
+		<option value="Благовещенск">Благовещенск</option>
+		<option value="Боровлянка">Боровлянка</option>
+		<option value="Бурла">Бурла</option>
+		<option value="Бурсоль">Бурсоль</option>
+		<option value="Волчиха">Волчиха</option>
+		<option value="Горняк">Горняк</option>
+		<option value="Ельцовка">Ельцовка</option>
+		<option value="Залесово">Залесово</option>
+		<option value="Заток">Заток</option>
+		<option value="Змеиногорск">Змеиногорск</option>
+		<option value="Ключи">Ключи</option>
+		<option value="Кош-Агач">Кош-Агач</option>
+		<option value="Красногорское">Красногорское</option>
+		<option value="Краснощеково">Краснощеково</option>
+		<option value="Кулунда">Кулунда</option>
+		<option value="Кытманово">Кытманово</option>
+		<option value="Мамонтово">Мамонтово</option>
+		<option value="Новичиха">Новичиха</option>
+		<option value="Онгудай">Онгудай</option>
+		<option value="Павловск">Павловск</option>
+		<option value="Петропавловское">Петропавловское</option>
+		<option value="Поспелиха">Поспелиха</option>
+		<option value="Ребриха">Ребриха</option>
+		<option value="Родино">Родино</option>
+		<option value="Смоленское">Смоленское</option>
+		<option value="Солонешное">Солонешное</option>
+		<option value="Солтон">Солтон</option>
+		<option value="Староаллейское">Староаллейское</option>
+		<option value="Табуны">Табуны</option>
+		<option value="Тальменка">Тальменка</option>
+		<option value="Топчиха">Топчиха</option>
+		<option value="Троицкое">Троицкое</option>
+
+
+
+
+
+
+
+<option value="3223">Амурская обл.</option>
+<option value="3251">Архангельская обл.</option>
+<option value="3282">Астраханская обл.</option>
+<option value="3296">Башкортостан (Башкирия)</option>
+<option value="3352">Белгородская обл.</option>
+<option value="5019396">Белоруссия</option>
+<option value="3371">Брянская обл.</option>
+<option value="3407">Бурятия</option>
+<option value="3437">Владимирская обл.</option>
+<option value="3468">Волгоградская обл.</option>
+<option value="3503">Вологодская обл.</option>
+<option value="3529">Воронежская обл.</option>
+<option value="3630">Дагестан</option>
+<option value="3673">Еврейская обл.</option>
+<option value="5555">Забайкальский край</option>
+<option value="3675">Ивановская обл.</option>
+<option value="3703">Иркутская обл.</option>
+<option value="3751">Кабардино-Балкария</option>
+<option value="5019395">Казахстан</option>
+<option value="3761">Калининградская обл.</option>
+<option value="3827">Калмыкия</option>
+<option value="3841">Калужская обл.</option>
+<option value="3872">Камчатская обл.</option>
+<option value="3892">Карелия</option>
+<option value="3921">Кемеровская обл.</option>
+<option value="3952">Кировская обл.</option>
+<option value="3994">Коми</option>
+<option value="4026">Костромская обл.</option>
+<option value="4052">Краснодарский край</option>
+<option value="4105">Красноярский край</option>
+<option value="4176">Курганская обл.</option>
+<option value="4198">Курская обл.</option>
+<option value="4227">Липецкая обл.</option>
+<option value="4243">Магаданская обл.</option>
+<option value="4270">Марий Эл</option>
+<option value="4287">Мордовия</option>
+<option value="4312" selected="">Москва и Московская обл.</option>
+<option value="4481">Мурманская обл.</option>
+<option value="3563">Нижегородская (Горьковская)</option>
+<option value="4503">Новгородская обл.</option>
+<option value="4528">Новосибирская обл.</option>
+<option value="4561">Омская обл.</option>
+<option value="4593">Оренбургская обл.</option>
+<option value="4633">Орловская обл.</option>
+<option value="4657">Пензенская обл.</option>
+<option value="4689">Пермская обл.</option>
+<option value="4734">Приморский край</option>
+<option value="4773">Псковская обл.</option>
+<option value="9999">Республика Крым</option>
+<option value="4800">Ростовская обл.</option>
+<option value="4861">Рязанская обл.</option>
+<option value="4891">Самарская обл.</option>
+<option value="4925">Санкт-Петербург и область</option>
+<option value="4969">Саратовская обл.</option>
+<option value="5011">Саха (Якутия)</option>
+<option value="5052">Сахалин</option>
+<option value="5080">Свердловская обл.</option>
+<option value="5151">Северная Осетия</option>
+<option value="5161">Смоленская обл.</option>
+<option value="5191">Ставропольский край</option>
+<option value="5225">Тамбовская обл.</option>
+<option value="5246">Татарстан</option>
+<option value="3784">Тверская обл.</option>
+<option value="5291">Томская обл.</option>
+<option value="5312">Тува (Тувинская Респ.)</option>
+<option value="5326">Тульская обл.</option>
+<option value="5356">Тюменская обл.</option>
+<option value="5404">Удмуртия</option>
+<option value="5432">Ульяновская обл.</option>
+<option value="5458">Уральская обл.</option>
+<option value="5473">Хабаровский край</option>
+<option value="2316497">Хакасия</option>
+<option value="2499002">Ханты-Мансийский АО</option>
+<option value="5507">Челябинская обл.</option>
+<option value="5543">Чечено-Ингушетия</option>
+<option value="5600">Чувашия</option>
+<option value="2415585">Чукотский АО</option>
+<option value="5019394">Ямало-Ненецкий АО</option>
+<option value="5625">Ярославская обл.</option>
