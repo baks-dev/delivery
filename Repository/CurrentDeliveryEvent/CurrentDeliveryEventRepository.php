001a8ad8a6b4b24da828ad29ab48e0e908238ef8
kingstar <admin@baks.dev>
2025-04-18 03:42:03 +0300

    Baks Development

--- Repository/CurrentDeliveryEvent/CurrentDeliveryEventRepository.php
+++ Repository/CurrentDeliveryEvent/CurrentDeliveryEventRepository.php
@@ -1,17 +1,17 @@
 <?php
 /*
- *  Copyright 2024.  Baks.dev <admin@baks.dev>
- *
+ *  Copyright 2025.  Baks.dev <admin@baks.dev>
+ *  
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
  *  in the Software without restriction, including without limitation the rights
  *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  *  copies of the Software, and to permit persons to whom the Software is furnished
  *  to do so, subject to the following conditions:
- *
+ *  
  *  The above copyright notice and this permission notice shall be included in all
  *  copies or substantial portions of the Software.
- *
+ *  
  *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  *  FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
@@ -25,25 +25,54 @@ declare(strict_types=1);
 
 namespace BaksDev\Delivery\Repository\CurrentDeliveryEvent;
 
+use BaksDev\Core\Doctrine\DBALQueryBuilder;
 use BaksDev\Core\Doctrine\ORMQueryBuilder;
 use BaksDev\Delivery\Entity\Delivery;
 use BaksDev\Delivery\Entity\Event\DeliveryEvent;
+use BaksDev\Delivery\Type\Event\DeliveryEventUid;
 use BaksDev\Delivery\Type\Id\DeliveryUid;
+use InvalidArgumentException;
 
 final class CurrentDeliveryEventRepository implements CurrentDeliveryEventInterface
 {
+    private DeliveryUid|false $delivery = false;
+
     public function __construct(
+        private readonly DBALQueryBuilder $DBALQueryBuilder,
         private readonly ORMQueryBuilder $ORMQueryBuilder
     ) {}
+
+    public function forDelivery(Delivery|DeliveryUid|string $delivery): self
+    {
+        if(empty($delivery))
+        {
+            $this->delivery = false;
+            return $this;
+        }
+
+        if(is_string($delivery))
+        {
+            $delivery = new DeliveryUid($delivery);
+        }
+
+        if($delivery instanceof Delivery)
+        {
+            $delivery = $delivery->getId();
+        }
+
+        $this->delivery = $delivery;
+
+        return $this;
+    }
 
     /**
      * Метод возвращает активное событие доставки
      */
-    public function get(DeliveryUid|string $delivery): ?DeliveryEvent
+    public function get(): ?DeliveryEvent
     {
-        if(is_string($delivery))
+        if(false === ($this->delivery instanceof DeliveryUid))
         {
-            $delivery = new DeliveryUid($delivery);
+            throw new InvalidArgumentException('Invalid Argument Delivery');
         }
 
         $qb = $this->ORMQueryBuilder->createQueryBuilder(self::class);
@@ -51,7 +80,11 @@ final class CurrentDeliveryEventRepository implements CurrentDeliveryEventInterf
         $qb
             ->from(Delivery::class, 'delivery')
             ->where('delivery.id = :delivery')
-            ->setParameter('delivery', $delivery, DeliveryUid::TYPE);
+            ->setParameter(
+                key: 'delivery',
+                value: $this->delivery,
+                type: DeliveryUid::TYPE
+            );
 
         $qb
             ->select('event')
@@ -63,5 +96,27 @@ final class CurrentDeliveryEventRepository implements CurrentDeliveryEventInterf
             );
 
         return $qb->getOneOrNullResult();
+    }
+
+    public function getId(): DeliveryEventUid|false
+    {
+        if(false === ($this->delivery instanceof DeliveryUid))
+        {
+            throw new InvalidArgumentException('Invalid Argument Delivery');
+        }
+
+        $dbal = $this->DBALQueryBuilder->createQueryBuilder(self::class);
+
+        $dbal
+            ->select('delivery.event AS value')
+            ->from(Delivery::class, 'delivery')
+            ->where('delivery.id = :delivery')
+            ->setParameter(
+                key: 'delivery',
+                value: $this->delivery,
+                type: DeliveryUid::TYPE
+            );
+
+        return $dbal->fetchHydrate(DeliveryEventUid::class);
     }
 }
--- UseCase/Admin/Delete/Tests/DeleteDeliveryHandleTest.php
+++ UseCase/Admin/Delete/Tests/DeleteDeliveryHandleTest.php
@@ -1,6 +1,6 @@
 <?php
 /*
- *  Copyright 2024.  Baks.dev <admin@baks.dev>
+ *  Copyright 2025.  Baks.dev <admin@baks.dev>
  *  
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
@@ -71,7 +71,9 @@ final class DeleteDeliveryHandleTest extends KernelTestCase
         $CurrentDeliveryEvent = $container->get(CurrentDeliveryEventRepository::class);
 
 
-        $DeliveryEvent = $CurrentDeliveryEvent->get(DeliveryUid::TEST);
+        $DeliveryEvent = $CurrentDeliveryEvent
+            ->forDelivery(DeliveryUid::TEST)
+            ->get();
         self::assertNotNull($DeliveryEvent);
 
 
