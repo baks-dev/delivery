d1d7e43512548d1e2d469a60a34e2b4f06cd295c
MichelAngelo <baksdevelopmen@gmail.com>
2025-07-14 01:03:58 +0300

    BaksDevelopment

--- Messenger/DeliveryMessage.php
+++ Messenger/DeliveryMessage.php
@@ -1,17 +1,17 @@
 <?php
 /*
- *  Copyright 2023.  Baks.dev <admin@baks.dev>
- *
+ *  Copyright 2025.  Baks.dev <admin@baks.dev>
+ *  
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
  *  in the Software without restriction, including without limitation the rights
  *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  *  copies of the Software, and to permit persons to whom the Software is furnished
  *  to do so, subject to the following conditions:
- *
+ *  
  *  The above copyright notice and this permission notice shall be included in all
  *  copies or substantial portions of the Software.
- *
+ *  
  *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  *  FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
@@ -31,43 +31,39 @@ use BaksDev\Delivery\Type\Id\DeliveryUid;
 final class DeliveryMessage
 {
     /** Идентификатор  */
-    private DeliveryUid $id;
+    private string $id;
 
     /** Идентификатор события  */
-    private DeliveryEventUid $event;
+    private string $event;
 
     /** Идентификатор предыдущего события  */
-    private ?DeliveryEventUid $last;
+    private string|false $last;
 
-    public function __construct(DeliveryUid $id, DeliveryEventUid $event, ?DeliveryEventUid $last = null)
+    public function __construct(DeliveryUid $id, DeliveryEventUid $event, DeliveryEventUid|null|false $last = false)
     {
-        $this->last = $last;
-        $this->id = $id;
-        $this->event = $event;
+        $this->id = (string) $id;
+        $this->event = (string) $event;
+        $this->last = empty($last) ? false : (string) $last;
     }
-
 
     /** Идентификатор  */
 
     public function getId(): DeliveryUid
     {
-        return $this->id;
+        return new DeliveryUid($this->id);
     }
-
 
     /** Идентификатор события  */
 
     public function getEvent(): DeliveryEventUid
     {
-        return $this->event;
+        return new DeliveryEventUid($this->event);
     }
-
 
     /** Идентификатор предыдущего события */
 
-    public function getLast(): ?DeliveryEventUid
+    public function getLast(): DeliveryEventUid|false
     {
-        return $this->last;
+        return $this->last ? new DeliveryEventUid($this->last) : false;
     }
-
 }
